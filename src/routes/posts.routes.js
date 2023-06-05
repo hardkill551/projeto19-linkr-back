91889d5fb38c42b48cfad5a07fdd1d04efdc7827
Hugo Costa <121950569+hardkill551@users.noreply.github.com>
2023-06-05 13:18:52 -0300

    Update posts.routes.js

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { createPost, getPosts,deletePost, updatePost,getUserPost } from "../controllers/posts.controller.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { postSchema } from "../schemas/posts.schemas.js";
-import {validateToken} from "../middlewares/validateToken.middleware.js";
+import {validateToken} from "../middlewares/auth.middleware.js";
 
 const postsRouter = Router();
 
