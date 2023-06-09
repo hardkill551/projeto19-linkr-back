c4cd2137efe9bf4432fdb2f5efb3b3a5c9cab4f1
hardkill551 <hardkill194@gmail.com>
2023-06-08 23:51:29 -0300

    fix/token

--- src/schemas/likes.schemas.js
+++ src/schemas/likes.schemas.js
@@ -2,5 +2,9 @@ import joi from "joi";
 
 export const likesSchema = joi.object({
     postId: joi.number().required(),
+    
+})
+export const postLikesSchema = joi.object({
+    postId: joi.number().required(),
     userId: joi.number().required()
 })
